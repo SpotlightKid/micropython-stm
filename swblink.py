24f55af80bb7be7f354a3f2184ccda3eedd0af6e
Christopher Arndt <chris@chrisarndt.de>
2015-05-15 15:15:35 +0200

    Fix line endings; coding style changes; move module level code into main

--- swblink.py
+++ swblink.py
@@ -5,26 +5,31 @@ Use USER switch to toggle blinking on off.
 
 """
 
-leds = [pyb.LED(i+1) for i in range(4)]
 sw_state = False
 
+
 def main():
+    leds = [pyb.LED(i+1) for i in range(4)]
+    sw1 = pyb.Switch()
+    sw1.callback(set_sw_state)
+
     while True:
         if sw_state:
-            for i in range(4):
-                leds[i].on()
+            for led in leds:
+                led.on()
                 pyb.delay(100)
-                leds[i].off()
+                led.off()
         else:
-            for i in range(4):
-                leds[i].off()
+            for led in leds:
+                leds.off()
+
             pyb.delay(200)
+
 
 def set_sw_state():
     global sw_state
     sw_state = not sw_state
 
+
 if __name__ == '__main__':
-    sw1 = pyb.Switch()
-    sw1.callback(set_sw_state)
     main()
